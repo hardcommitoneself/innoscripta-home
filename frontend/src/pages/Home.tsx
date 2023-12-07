61c296e32e0d5f2cecab3a3a5d09e3a286b3e9dd
hardcommitoneself <hardcommitoneself@gmail.com>
2023-12-07 20:14:04 +1000

    font: hot fix on UI

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { ArticleCard, TrendingCard, Input, Button } from "components";
+import { ArticleCard, TrendingCard, Input, Button, Card } from "components";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const articles = [
+  {
+    imgUrl: "https://picsum.photos/id/237/200/300",
+    title: "The overlooked benefits of real Christmas trees",
+    description:
+      "The environmental pros and cons of Christmas trees go far beyond the asdf adsf asdkjfljs d sskjd fjslkjd fkaskdf jaklsjd flaks jfasjdlkj asdj kj",
+    author: "Rey",
+    source: "Green",
+    category: "Environment",
+    publishedAt: "2023-12-01",
+  },
+  {
+    imgUrl: "https://picsum.photos/id/237/200/300",
+    title: "The overlooked benefits of real Christmas trees",
+    description:
+      "The environmental pros and cons of Christmas trees go far beyond the asdf adsf asdkjfljs d sskjd fjslkjd fkaskdf jaklsjd flaks jfasjdlkj asdj kj",
+    author: "Rey",
+    source: "Green",
+    category: "Environment",
+    publishedAt: "2023-12-01",
+  },
+];
+
+const trends = [
   {
     imgUrl: "https://picsum.photos/id/237/200/300",
     title: "The overlooked benefits of real Christmas trees",
@@ -28,17 +51,31 @@ export const Home = () => {
       </div>
 
       <div className="flex gap-10 mt-5">
-        <div className="flex gap-5 w-2/3">
+        <div className="flex gap-5 w-3/5">
           {/* blog list */}
           <div className="flex flex-col gap-4">
             {articles.map((article, index) => (
-              <ArticleCard {...article} />
+              <div key={index}>
+                <ArticleCard {...article} />
+              </div>
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-10 w-1/3">
+        <div className="flex flex-col gap-6 w-2/5">
           {/* trending */}
-          <TrendingCard />
+          <Card>
+            <div className="flex flex-col gap-3">
+              <h1 className="text-base font-bold">Trending</h1>
+
+              <div className="flex flex-col gap-3">
+                {trends.map((trend, index) => (
+                  <div key={index}>
+                    <TrendingCard {...trend} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </Card>
 
           {/* recommended users */}
         </div>
